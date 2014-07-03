38267ca3a185b0ff7af7934ca2206b4985fd925a
Raymond Usbal <raymond@philippinedev.com>
2014-07-03 12:15:12 +0800

    Model image.user from optional to required

--- imagestore/models/bases/image.py
+++ imagestore/models/bases/image.py
@@ -49,7 +49,7 @@ class BaseImage(models.Model):
     tags = TagField(_('Tags'), blank=True)
     order = models.IntegerField(_('Order'), default=0)
     image = ImageField(verbose_name = _('File'), upload_to=get_file_path)
-    user = models.ForeignKey(User, verbose_name=_('User'), null=True, blank=True, related_name='images')
+    user = models.ForeignKey(User, verbose_name=_('User'), related_name='images')
     created = models.DateTimeField(_('Created'), auto_now_add=True, null=True)
     updated = models.DateTimeField(_('Updated'), auto_now=True, null=True)
     album = models.ForeignKey(get_model_string('Album'), verbose_name=_('Album'), null=True, blank=True, related_name='images')

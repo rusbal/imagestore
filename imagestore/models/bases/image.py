871db9c6632865c0196741d4d768d900972086d9
Raymond Usbal <raymond@philippinedev.com>
2014-07-03 12:19:03 +0800

    Model image.album from optional to required

--- imagestore/models/bases/image.py
+++ imagestore/models/bases/image.py
@@ -52,7 +52,7 @@ class BaseImage(models.Model):
     user = models.ForeignKey(User, verbose_name=_('User'), related_name='images')
     created = models.DateTimeField(_('Created'), auto_now_add=True, null=True)
     updated = models.DateTimeField(_('Updated'), auto_now=True, null=True)
-    album = models.ForeignKey(get_model_string('Album'), verbose_name=_('Album'), null=True, blank=True, related_name='images')
+    album = models.ForeignKey(get_model_string('Album'), verbose_name=_('Album'), related_name='images')
 
     @permalink
     def get_absolute_url(self):

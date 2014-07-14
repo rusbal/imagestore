15bd5d6376a4d064ba155af245827fd586400cba
Raymond Usbal <raymond@philippinedev.com>
2014-07-14 23:54:01 +0800

    Hide thumb preview when adding image

--- imagestore/static/imagestore/admin/js/add-image.js
+++ imagestore/static/imagestore/admin/js/add-image.js
@@ -3,5 +3,10 @@
     if (n <= 2) {
         $('select#id_user option:last').attr('selected', 'selected');
     }
+
+    /**
+     * Hide thumb preview when adding image
+     */
+    $('input#id_mediafile').remove();
 });})(django.jQuery);
 

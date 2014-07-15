02ab78daac7954be37971ae07a7f0eeba43255fb
Raymond Usbal <raymond@philippinedev.com>
2014-07-15 14:12:17 +0800

    Ajax: get_image_to_owner

--- imagestore/ajax.py
+++ imagestore/ajax.py
@@ -5,6 +5,18 @@ from django.utils.safestring import mark_safe
 from .models import Image
 
 
+def get_image_to_owner(request):
+    try:
+        images = Image.objects.all()
+        img2owner = {}
+        for image in images:
+            img2owner[image.pk] = image.user.pk
+        success = True
+    except:
+        success = False
+    return HttpResponse(json.dumps({'success': success, 'image_owner': img2owner}), mimetype="application/json")
+
+
 def get_image_thumbs(request):
     try:
         images = Image.objects.all()
